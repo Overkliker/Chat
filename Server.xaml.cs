c08f60b2b271ffdf5b771085ae4ccce04c6abcf3
Danil <danilahreno@yandex.ru>
2023-05-02 22:24:02 +0300

    Добавьте файлы проекта.

---
+++ Server.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Chat
+{
+    /// <summary>
+    /// Логика взаимодействия для Server.xaml
+    /// </summary>
+    public partial class Server : Window
+    {
+        private Socket socket;
+        private List<Socket> clients = new List<Socket>();
+        public Server()
+        {
+            InitializeComponent();
+            IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, 8888);
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            socket.Bind(ipPoint);
+            socket.Listen(1000);
+            ListenClients();
+        }
+
+        private async Task ListenClients()
+        {
+            while (true)
+            {
+                var client = await socket.AcceptAsync();
+                clients.Add(client);
+            }
+        }
+
+        private async Task ReciveMessage(Socket client)
+        {
+            while (true)
+            {
+                byte[] bytes = new byte[1024];
+                await client.ReceiveAsync(bytes, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(bytes);
+                MessagesLbx.Items.Add($"[Телеграмма от {client.RemoteEndPoint}]: {message}");
+
+                foreach (var user in clients)
+                {
+                    SendMessage(user, message);
+                }
+            }
+        }
+
+        private async Task SendMessage(Socket client, string message)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(bytes, SocketFlags.None);
+        }
+    }
+}
